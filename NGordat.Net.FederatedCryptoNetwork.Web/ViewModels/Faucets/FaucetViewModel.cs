35a523ebe7c05c3cb40dceb33573e4f63d34ecbc
Land3r <nicolas.gordat@gmail.com>
2021-07-28 22:18:05 +0200

    Upd

---
+++ NGordat.Net.FederatedCryptoNetwork.Web/ViewModels/Faucets/FaucetViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NGordat.Net.FederatedCryptoNetwork.Web.ViewModels.Faucets
+{
+    public class FaucetViewModel
+    {
+        public string Name { get; set; }
+    }
+}
