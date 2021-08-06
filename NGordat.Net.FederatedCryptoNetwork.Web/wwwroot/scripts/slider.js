151d4e11d2de281a73adc6ea8c4bfa4ac255517c
Land3r <nicolas.gordat@gmail.com>
2021-08-06 18:08:27 +0200

    upd slider

---
+++ NGordat.Net.FederatedCryptoNetwork.Web/wwwroot/scripts/slider.js
@@ -0,0 +1,11 @@
+﻿var slide = new Array("/images/faucets/faucetcrypto/1.png", "/images/faucets/faucetcrypto/2.png", "/images/faucets/faucetcrypto/3.png");
+var numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
+}
