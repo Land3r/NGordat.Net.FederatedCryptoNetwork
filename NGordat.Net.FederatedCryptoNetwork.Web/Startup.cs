7a0827c067861709cfc9dcb56546dbb5b861ce5b
Land3r <nicolas.gordat@gmail.com>
2021-08-09 18:03:50 +0200

    HCaptcha

--- NGordat.Net.FederatedCryptoNetwork.Web/Startup.cs
+++ NGordat.Net.FederatedCryptoNetwork.Web/Startup.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using BenjaminAbt.HCaptcha.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -63,6 +63,8 @@ namespace NGordat.Net.FederatedCryptoNetwork.Web
 
             DalConfigurationExtensions.Configure(services);
             ServicesConfigurationExtensions.Configure(services);
+
+            services.AddHCaptcha(Configuration.GetSection("HCaptcha"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
